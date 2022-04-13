0dec4df591c32312005e2689d97cb40e9a179914
siraj4444 <64959178+siraj4444@users.noreply.github.com>
2022-04-13 15:47:19 +0530

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace ApiSiraj.Repository.Models
+{
+    public partial class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [StringLength(50)]
+        public string Email { get; set; }
+        [StringLength(10)]
+        public string Phone { get; set; }
+        [StringLength(50)]
+        public string Admin { get; set; }
+        public bool? Status { get; set; }
+    }
+}
