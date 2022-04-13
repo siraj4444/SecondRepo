8a32f94eafd9b0f8b32e3de5e6c993eb0b9438ca
siraj4444 <64959178+siraj4444@users.noreply.github.com>
2022-04-13 15:48:36 +0530

    Added Context Class

---
+++ EmployeeDbContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using ApiSiraj.Repository.Models;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace ApiSiraj.Repository
+{
+    public partial class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext()
+        {
+        }
+
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<EmployeesSiraj> EmployeesSiraj { get; set; }
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-AOFFF26;Initial Catalog=UserManagement;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EmployeesSiraj>(entity =>
+            {
+                entity.HasKey(e => e.EmployeeId)
+                    .HasName("PK__Employee__7AD04F11F4A55BEA");
+
+                entity.Property(e => e.State).IsUnicode(false);
+
+                entity.Property(e => e.SteetNo).IsUnicode(false);
+
+                entity.Property(e => e.StreetName).IsUnicode(false);
+
+                entity.Property(e => e.Town).IsUnicode(false);
+
+                entity.Property(e => e.UnitOrApt).IsUnicode(false);
+
+                entity.Property(e => e.ZipCode).IsUnicode(false);
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Admin).IsUnicode(false);
+
+                entity.Property(e => e.Email).IsUnicode(false);
+
+                entity.Property(e => e.FirstName).IsUnicode(false);
+
+                entity.Property(e => e.LastName).IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
